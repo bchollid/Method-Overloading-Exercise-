b898c28b847b9d585ea272462518c2a0aadc0683
bchollid <brandonholliday1@gmail.com>
2024-01-03 08:37:05 -0700

    Finished

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,44 @@
 {
     public class Program
     {
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool canAdd)
+        {
+            if(canAdd == true)
+            {
+                int sum = a + b;
+                if(sum == 1)
+                {
+                    return $"{sum} dollar.";
+                }
+                else if (sum > 1)
+                {
+                    return $"{sum} dollars.";
+                }
+            }
+            else
+            {
+                return "Sorry, I can't add those numbers.";
+            }
+            return "Please try again.";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 1));
+            Console.WriteLine(Add(1.111, 1.111));
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(1, 1, true));
+            Console.WriteLine(Add(1, 1, false));
         }
     }
 }
